6215b55c14024a824a0f7ffbd075394cb032b718
boian-ivanov <boian.ivanov44@gmail.com>
2017-08-16 07:05:27 +0300

    Work on DB methods - Created select method in DB.php - Logic for db calls will be stackable ex. $this->db->select()->where()->exec(); TODO - Expand previous said logic to more db methods

--- app/model/home/common/index.php
+++ app/model/home/common/index.php
@@ -4,5 +4,6 @@ class ModelCommonIndex extends Model{
 
     public function index() {
         echo "Fuckin model";
+        var_dump($this->db->select(['col1', 'col2', 'col3'], 'test')->where('')->exec());
     }
 }
