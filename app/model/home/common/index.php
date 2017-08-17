784761bcbbca4aa97959f80db484d023621255a2
boian-ivanov <boian.ivanov44@gmail.com>
2017-08-17 15:42:39 +0300

    - Added Db::create method - Added dump method for debugging purposes

--- app/model/home/common/index.php
+++ app/model/home/common/index.php
@@ -4,7 +4,19 @@ class ModelCommonIndex extends Model{
 
     public function index() {
         echo "Fuckin model";
-        var_dump($this->db->select('test', ['col1', 'col2', 'col3'])->where(['col1 = 1', 'col2 = 2'])->exec());
+        var_dump($this->db->create([
+            'name' => 'x',
+            'cols' => [
+                'id' => 'INT(11) NOT NULL',
+                'qwe' => 'INT(11) NOT NULL',
+                'asd' => 'INT(11) NOT NULL',
+                'zxc' => 'INT(11) NOT NULL'
+            ],
+            'engine' => 'myisam',
+            'debug' => true
+        ]));
+//        var_dump($this->db->select('test', ['col1', 'col2', 'col3'])->where(['col1 = 1', 'col2 = 2'])->exec());
+//        var_dump($this->db->select('test', ['col1', 'col2', 'col3'])->where(['col1 = 1', 'col2 = 2'])->dump());
         //var_dump($this->db->query('SELECT * FROM test')->exec());
     }
 }
