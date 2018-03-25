ef58e89a7a289aaaf2b6b2c5f56792299ec66885
boian-ivanov <boian.ivanov44@gmail.com>
2018-03-25 20:50:47 +0100

    Changelog :  - Created a presentable admin login page.  - Added a URL methods to to get root url.  - Changed main logo image from .jpg to .svg.  - Commented URL methods that outputted dynamic urls.

--- app/admin/controller/common/index.php
+++ app/admin/controller/common/index.php
@@ -1,4 +1,3 @@
-
 <?php
 
 class ControllerCommonIndex extends Controller{
@@ -10,34 +9,36 @@ class ControllerCommonIndex extends Controller{
 
         $this->head->addLinks([
             'rel'  => 'stylesheet',
-            'href' => '/public/css/bootstrap.min.css'
+            'href' => $this->url->root . '/public/css/bootstrap.min.css'
         ]);
 
         $this->head->addLinks([
             'rel'  => 'stylesheet',
-            'href' => '/public/css/font-awesome.min.css'
+            'href' => $this->url->root . '/public/css/font-awesome.min.css'
         ]);
 
-        $this->head->addScript('public/js/jquery-3.2.1.min.js');
-        $this->head->addScript('public/js/bootstrap.min.js');
+        $this->head->addScript($this->url->root . '/public/js/jquery-3.2.1.min.js');
+        $this->head->addScript($this->url->root . '/public/js/bootstrap.min.js');
 
         $data['scripts'] = $this->head->getScripts();
         $data['links'] = $this->head->getLinks();
 
-        $data['form_link'] = $this->full_url($_SERVER) . '/common/login';
-        $data['forgotten_link'] = $this->full_url($_SERVER) . '/common/reset';
+        $data['form_link'] = $this->url->root . '/admin/common/login';
+        $data['forgotten_link'] = $this->url->root . '/admin/common/reset';
+
+        $data['root_url'] = $this->url->root;
 
         /*  TODO : URL library for url generation
         $data['form_link'] = $this->url->generate('common/login') . 'common/login';*/
 
         $data['hello'] = "Welcome to SCU Framework Admin Panel";
 
+        $data['data'] = "You have entered the admin panel";
+
         $data['header'] = $this->load->view('common/header', $data);
 
         $data['footer'] = $this->load->view('common/footer', $data);
 
-        $data['data'] = "You have entered the admin panel";
-
-        echo $this->load->view('common/index', $data);
+        return $this->load->view('common/index', $data);
     }
 }
--- system/library/url.php
+++ system/library/url.php
@@ -3,5 +3,14 @@
 
 class Url {
 
-    
+    public $root;
+
+    public function __construct() {
+        $this->setDefaults();
+    }
+
+    public function setDefaults() {
+        $protocol = isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != null ? 'https://' : 'http://';
+        $this->root = $protocol . $_SERVER['HTTP_HOST'];
+    }
 }
