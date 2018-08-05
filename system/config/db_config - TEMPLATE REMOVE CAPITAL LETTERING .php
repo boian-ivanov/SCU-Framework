9f03b91a3c28075c72989921c7cb67e7995e3802
boian-ivanov <boian.ivanov44@gmail.com>
2018-08-05 23:07:15 +0100

    Removed default db_config and added a template one with explanations for each field.

---
+++ system/config/db_config - TEMPLATE REMOVE CAPITAL LETTERING .php
@@ -0,0 +1,8 @@
+<?php
+define('DB_DRIVER', 'mysql'); // Database driver, default one set to mysql
+define('DB_HOSTNAME', ''); // Name of host server, default in a local environment is "localhost"
+define('DB_USERNAME', ''); // Username for login, default in a local environment is "root"
+define('DB_PASSWORD', ''); // Password for login, default in a local environment is ""
+define('DB_DATABASE', ''); // Name of the used database
+define('DB_PORT', ''); // Default database port is 3306
+define('DB_PREFIX', ''); // Database prefix in front of every existing table
--- system/config/db_config.php
+++ system/config/db_config.php
@@ -1,8 +1,8 @@
 <?php
-//define('DB_DRIVER', 'mysqli');
+define('DB_DRIVER', 'mysql');
 define('DB_HOSTNAME', 'localhost');
-define('DB_USERNAME', '');
+define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
-define('DB_DATABASE', '');
+define('DB_DATABASE', 'test');
 define('DB_PORT', '3306');
-define('DB_PREFIX', '');
+define('DB_PREFIX', 'tst_');
