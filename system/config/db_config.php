5bd2511e8fbbe7dc9f53523287daadc3dd2a836f
boian-ivanov <boian.ivanov44@gmail.com>
2018-10-23 14:45:15 +0000

    Emitted used db_config.php file.

--- system/config/db_config.php
+++
@@ -1,8 +0,0 @@
-<?php
-define('DB_DRIVER', 'mysql');
-define('DB_HOSTNAME', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_DATABASE', 'test');
-define('DB_PORT', '3306');
-define('DB_PREFIX', 'tst_');
